90767f7ba1264f4cd5afa78c927ccec930807fb8
mdshahniaj-a11y <mdshahniaj@gmail.com>
2026-09-21 22:45:00 -0700

    third commit

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,45 +1,45 @@
-// import logo from "../assets/logo-text.png";
-// const Nav = () => {
-//     return (
-//         <div className="border-b border-gray-200 bg-white sticky top-0 z-50">
+import logo from "../assets/logo-text.png";
+const Nav = () => {
+    return (
+        <div className="border-b border-gray-200 bg-white sticky top-0 z-50">
 
-//             <div className="flex items-center justify-between px-10 py-4 container mx-auto">
+            <div className="flex items-center justify-between px-10 py-4 container mx-auto">
 
-//                 {/* Logo */}
-//                 <div className="flex items-center gap-2">
-//                     <img src={logo} alt="" />
-//                 </div>
-
-
-//                 {/* Menu */}
-//                 <div className="flex gap-6">
-//                     <a className="text-pink-500" href="#">
-//                         Home
-//                     </a>
-
-//                     <a href="#">Technologies</a>
-
-//                     <a href="#">Projects</a>
-
-//                     <a href="#">About</a>
-
-//                     <a href="#">Contact</a>
-//                 </div>
+                {/* Logo */}
+                <div className="flex items-center gap-2">
+                    <img src={logo} alt="" />
+                </div>
 
 
-//                 {/* Button */}
-//                 <div className="flex items-center gap-4">
-//                     <a href="#">Sign In</a>
+                {/* Menu */}
+                <div className="flex gap-6">
+                    <a className="text-pink-500" href="#">
+                        Home
+                    </a>
 
-//                     <button className="bg-pink-500 text-white px-4 py-2 rounded-full">
-//                         Sign Up
-//                     </button>
-//                 </div>
+                    <a href="#">Technologies</a>
 
-//             </div>
+                    <a href="#">Projects</a>
 
-//         </div>
-//     );
-// };
+                    <a href="#">About</a>
 
-// export default Nav;
+                    <a href="#">Contact</a>
+                </div>
+
+
+                {/* Button */}
+                <div className="flex items-center gap-4">
+                    <a href="#">Sign In</a>
+
+                    <button className="bg-pink-500 text-white px-4 py-2 rounded-full">
+                        Sign Up
+                    </button>
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Nav;
